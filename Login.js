48039bf14686c832136781df096b6aded5b13d0d
anandhu-as <anandhuas010203@gmail.com>
2023-05-31 13:45:52 +0530

    password validation

--- Login.js
+++ Login.js
@@ -4,13 +4,20 @@ const form = document.getElementById("form");
 const errorDiv = document.getElementById("error");
 form.addEventListener("submit", (e) => {
   let errorMessages = [];
-  name.value === "" || name.value == null?
-    errorMessages.push("Name is required"):null
+  password.value === "password"
+    ? errorMessages.push("password cannot be password")
+    : null;
+  name.value === "" || name.value == null
+    ? errorMessages.push("Name is required")
+    : null;
   password.value.length <= 6
     ? errorMessages.push("password must be longer than 6 characters")
     : null;
+  password.value.length >= 12
+    ? errorMessages.push("password must be less than 12 characters")
+    : "";
   if (errorMessages.length > 0) {
     e.preventDefault();
-    errorDiv.innerText = errorMessages.join(",");
+    errorDiv.innerText = errorMessages.join(" , ");
   }
 });
